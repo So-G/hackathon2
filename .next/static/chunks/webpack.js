e50a6aaf1a0855f9ebfc0bb6481a7420428c75de
Solene G <solene.gerard@gmail.com>
2021-06-28 15:43:08 +0200

    deleted 3 company divs in index file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5913eec5fed9f271341f"; }
+/******/ 		__webpack_require__.h = function() { return "99873e5a18796376d595"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
