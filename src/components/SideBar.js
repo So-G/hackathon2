7cc34ad98bb099a5fb65db05e762662bab6902d0
Solene G <solene.gerard@gmail.com>
2021-06-29 15:25:49 +0200

    fixed sidebar component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,7 +3,9 @@ export default function SideBar() {
     <div className="sidebar">
       <h3 className="product-owners">They are looking for people</h3>
       <ul>
-        <li>Product Owner</li>
+        <li>Product Owner 1</li>
+        <li>Product Owner 2</li>
+        <li>Product Owner 3</li>
       </ul>
     </div>
   );
