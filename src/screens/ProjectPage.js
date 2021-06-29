2f184e996bd6eddb224c87e196ef81057d95b763
Solene G <solene.gerard@gmail.com>
2021-06-29 15:34:55 +0200

    deleted bubble component

--- src/screens/ProjectPage.js
+++ src/screens/ProjectPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Bubbles from '../components/Bubbles';
 import '../styles/main.css';
 
 export default function ProjectPage() {
   return (
     <>
       <h1>Projects</h1>
-      <Bubbles />
     </>
   );
 }
