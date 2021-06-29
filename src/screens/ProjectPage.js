ecee626903cd293c62e0bee16214078889fe7daf
florian.J <knives38@gmail.com>
2021-06-29 15:50:31 +0200

    les bulles c'est trop bien!!

--- src/screens/ProjectPage.js
+++ src/screens/ProjectPage.js
@@ -1,35 +1,28 @@
-/* eslint-disable no-lone-blocks */
 // import axios from "axios";
+import { Link } from 'react-router-dom';
 import './ProjectPage.css';
 
 export default function ProjectPage() {
-  {
-    /* const getProjet = () => {
-    axios({
-      method:'GET',
-      url: ''
-    })
-  // eslint-disable-next-line prettier/prettier
-  } */
-  }
   return (
     <div className="bg-grey-200 w-screen ">
       <div className=" ">
         <h1>Projects</h1>
         <div className=" sm:flex md:flex-row justify-around h-4/5">
-          <div className="  my-32 bg-green-600 h-52 w-52 p-10 justify-items-center align-center rounded-full transform hover:shadow-2xl transition duration-500 transform hover:scale-110 transition duration-500 shadow-lg">
+          <Link to="/projects/:id">
+            <div className="  my-32 bg-green-600 h-52 w-52 p-8 items-center align-center rounded-full transform hover:shadow-2xl transition duration-500 transform hover:scale-110 transition duration-500 shadow-lg">
+              <h3>Projet A</h3>
+              <p>Marketing 1/3</p>
+              <p>Social media Manager 1/1</p>
+              <p>Devs 2/5</p>
+            </div>
+          </Link>
+          <div className=" m-16 bg-pink-400 h-52 w-52 p-10 items-center align-center rounded-full transform hover:shadow-2xl transition duration-500 transform hover:scale-110 transition duration-500 shadow-lg">
             <h3>Projet X</h3>
-            <p>Marketing 1/3</p>
-            <p>Social media Manager 1/1</p>
+            <p>UX/UI 1/1</p>
+            <p>Devops 1/1</p>
             <p>Devs 2/5</p>
           </div>
-          <div className=" m-16 bg-pink-400 h-52 w-52 p-10 justify-items-center align-center rounded-full transform hover:shadow-2xl transition duration-500 transform hover:scale-110 transition duration-500 shadow-lg">
-            <h3>Projet X</h3>
-            <p>Marketing 1/3</p>
-            <p>Social media Manager 1/1</p>
-            <p>Devs 2/5</p>
-          </div>
-          <div className=" my-72 bg-blue-300 h-52 w-52 p-10 justify-items-center align-center rounded-full transform hover:shadow-2xl transition duration-500 transform hover:scale-110 transition duration-500 shadow-lg">
+          <div className=" my-72 bg-blue-300 h-52 w-52 p-10 items-center align-center rounded-full transform hover:shadow-2xl transition duration-500 transform hover:scale-110 transition duration-500 shadow-lg">
             <h3>Projet B</h3>
             <p>Marketing 1/3</p>
             <p>Social media Manager 1/1</p>
