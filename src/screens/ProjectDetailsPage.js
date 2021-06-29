a324b94032607aab36544b6400c5af8b1902c28e
Solene G <solene.gerard@gmail.com>
2021-06-29 18:46:01 +0200

    email sent

--- src/screens/ProjectDetailsPage.js
+++ src/screens/ProjectDetailsPage.js
@@ -71,9 +71,10 @@ export default function ProjectDetailsPage() {
             />
             <br />
             <button
-              type="submit"
+              type="button"
               className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={sendEmail}
+              // eslint-disable-next-line no-alert
+              onSubmit={() => alert('Email successfully sent 💌')}
             >
               Send invitation
             </button>
