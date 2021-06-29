04f70b301dd2878cbad356bdfa136c727fb01d1f
florian.J <knives38@gmail.com>
2021-06-29 17:09:08 +0200

    fix all merge

---
+++ src/components/SideBar.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+      <h3 className="product-owners">They are looking for people</h3>
+      <ul>
+        <li>Product Owner 1</li>
+        <li>Product Owner 2</li>
+        <li>Product Owner 3</li>
+      </ul>
+    </div>
+  );
+}
