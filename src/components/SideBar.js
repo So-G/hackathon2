f60867f2edd40769075c5bde86eb12a8ed6b94e3
Heranca <heranca.kamalo@gmail.com>
2021-06-29 18:45:47 +0200

    home page css

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 export default function SideBar() {
   return (
-    <div className="sidebar">
+    <div className="p-5 border-l-2 border-gray-600">
       <h3 className="product-owners">They are looking for people</h3>
       <ul>
         <li>Product Owner 1</li>
